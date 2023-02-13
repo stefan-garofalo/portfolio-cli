d10fe65e5a6691b7a11d99fe390f70851138c8cd
stefan-garofalo <stefan.garofalo@promemoriagroup.com>
2023-02-13 14:30:43 +0100

    fix: terminal label

--- src/components/terminal.jsx
+++ src/components/terminal.jsx
@@ -1,7 +1,7 @@
 export default function Terminal() {
   return (
     <div className="mx-48">
-      <span>Speaking of which... a <b>simpler</b> and <b>faster</b> approach to explore</span>
+      <span>Speaking of which... a <b>simpler</b> and <b>faster</b> approach to get right to the point</span>
       <div className="h-[45vh] overflow-y-auto  rounded-xl border border-slate-500">
       </div>
     </div>
