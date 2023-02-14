4137fbed07220f178341be08822f86749e3d3e5a
StefanGarofalo <garofalo.s@outlook.com>
2023-02-14 01:27:05 +0100

    feat: typing cursor and framer motion

--- src/components/UI/hero.jsx
+++ src/components/UI/hero.jsx
@@ -1,9 +1,21 @@
+import { TypeAnimation } from "react-type-animation"
+
 export default function Hero() {
   return (
     <div className={`flex-center flex-col py-16`}>
-      <h1 className='mb-3 text-7xl font-extrabold'>Stefan Garofalo</h1>
-      <h2 className='mb-7 text-xl'>FRONT END DEVELOPER, TECH ENTHUSIAST</h2>
-      <p className='w-1/2 text-center prose-2xl'>
+      <TypeAnimation
+        sequence={['Stefan Garofalo']}
+        wrapper="h1"
+        className="mb-3 text-7xl font-extrabold"
+        cursor={false}
+      />
+      <TypeAnimation
+        sequence={['FRONT END DEVELOPER, TECH ENTHUSIAST']}
+        wrapper="h2"
+        className="mb-10 text-xl"
+        cursor={false}
+      />
+      <p className='w-1/2 text-center prose-xl'>
         Ever in love with <span className='font-bold text-turquoise'>simple</span> yet <span className='font-bold text-purple'>effective</span> solutions.
         <br/>
         I strive to create <span className='font-bold text-blue'>state of the art</span> web applications making use of the <span className='font-bold text-green'>latest</span> technologies,
