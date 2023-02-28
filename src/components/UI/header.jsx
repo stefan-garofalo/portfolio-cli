ce0cd513d48f4e5b299e7dc6f73387a8cada0780
stefan-garofalo <stefan.garofalo@promemoriagroup.com>
2023-02-28 17:59:06 +0100

    feat: active link underline

--- src/components/UI/header.jsx
+++ src/components/UI/header.jsx
@@ -1,13 +1,25 @@
 import Link from "next/link"
+import { useRouter } from "next/router"
 
 export default function Header({ className }) {
+  const router = useRouter()
+  const hash = router.asPath.split('#').at(-1)
   return (
-    <header className="sticky top-0 w-full flex-between py-6 px-10 backdrop-blur-md bg-black/20 border-b border-slate-500 text-xl">
+    <header className="sticky top-0 w-full flex-between py-6 px-10 backdrop-blur-md bg-black/20 border-b border-slate-500 text-lg">
       <div>Find me on:</div>
       <nav className="flex gap-12 font-bold">
-        <Link href="#experience" className="border-b-[3px] border-transparent transition-all hover:border-purple">Experience</Link>
-        <Link href="#projects" className="border-b-[3px] border-transparent transition-all hover:border-blue">Projects</Link>
-        <Link href="#skills" className="border-b-[3px] border-transparent transition-all hover:border-green">Skills</Link>
+        <Link href="#experience" className={`
+          border-b-[3px] border-transparent transition-all hover:border-purple
+          ${hash === 'experience' && 'border-purple'}
+        `}>Experience</Link>
+        <Link href="#projects" className={`
+          border-b-[3px] border-transparent transition-all hover:border-blue
+          ${hash === 'projects' && 'border-blue'}
+        `}>Projects</Link>
+        <Link href="#skills" className={`
+          border-b-[3px] border-transparent transition-all hover:border-green
+          ${hash === 'skills' && 'border-green'}
+        `}>Skills</Link>
       </nav>
     </header>
   )
